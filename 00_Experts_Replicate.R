90350bd851757d3bd2ad07bada2c3b01c4f02619
Peter King <59886014+pmpk20@users.noreply.github.com>
2025-02-12 22:02:14 +0000

    12/02: Adding numbered scripts.

---
+++ 00_Experts_Replicate.R
@@ -0,0 +1,218 @@
+#### Experts: Execute all paper code ####
+## Author: Dr Peter King (p.m.king@kent.ac.uk)
+## Last change: 10/02/2025
+## TODO: Format nicely
+# -
+
+
+# ******************************************************************
+# Replication Information: ####
+# ******************************************************************
+
+
+# ─ Session info ──────────────────────────────────────────────────────────────
+# setting  value
+# version  R version 4.4.1 (2024-06-14 ucrt)
+# os       Windows 11 x64 (build 22631)
+# system   x86_64, mingw32
+# ui       RStudio
+# language (EN)
+# collate  English_United Kingdom.utf8
+# ctype    English_United Kingdom.utf8
+# tz       Europe/London
+# date     2025-02-12
+# rstudio  2023.06.2+561 Mountain Hydrangea (desktop)
+# pandoc   NA
+#
+# ─ Packages ──────────────────────────────────────────────────────────────────
+# package        * version    date (UTC) lib source
+# apollo         * 0.3.4      2024-10-01 [1] CRAN (R 4.4.1)
+# backports        1.5.0      2024-05-23 [1] CRAN (R 4.4.0)
+# bgw              0.1.3      2024-03-29 [1] CRAN (R 4.4.0)
+# chk              0.9.1      2023-10-06 [1] CRAN (R 4.4.0)
+# cli              3.6.3      2024-06-21 [1] CRAN (R 4.4.1)
+# cobalt         * 4.5.5      2024-04-02 [1] CRAN (R 4.4.0)
+# coda             0.19-4.1   2024-01-31 [1] CRAN (R 4.4.0)
+# colorspace       2.1-0      2023-01-23 [1] CRAN (R 4.4.0)
+# crayon           1.5.3      2024-06-20 [1] CRAN (R 4.4.1)
+# data.table     * 1.15.4     2024-03-30 [1] CRAN (R 4.4.0)
+# digest           0.6.35     2024-03-11 [1] CRAN (R 4.4.0)
+# distributional * 0.4.0      2024-02-07 [1] CRAN (R 4.4.0)
+# dplyr          * 1.1.4      2023-11-17 [1] CRAN (R 4.4.0)
+# fansi            1.0.6      2023-12-08 [1] CRAN (R 4.4.0)
+# forcats        * 1.0.0      2023-01-29 [1] CRAN (R 4.4.0)
+# generics         0.1.3      2022-07-05 [1] CRAN (R 4.4.0)
+# ggdist         * 3.3.2      2024-03-05 [1] CRAN (R 4.4.0)
+# ggplot2        * 3.5.1      2024-04-23 [1] CRAN (R 4.4.0)
+# ggridges       * 0.5.6      2024-01-23 [1] CRAN (R 4.4.0)
+# glue             1.7.0      2024-01-09 [1] CRAN (R 4.4.0)
+# gridExtra      * 2.3        2017-09-09 [1] CRAN (R 4.4.0)
+# gtable           0.3.5      2024-04-22 [1] CRAN (R 4.4.0)
+# here           * 1.0.1      2020-12-13 [1] CRAN (R 4.4.0)
+# hms              1.1.3      2023-03-21 [1] CRAN (R 4.4.0)
+# lattice          0.22-6     2024-03-20 [1] CRAN (R 4.4.1)
+# lifecycle        1.0.4      2023-11-07 [1] CRAN (R 4.4.0)
+# lubridate      * 1.9.3      2023-09-27 [1] CRAN (R 4.4.0)
+# magrittr       * 2.0.3      2022-03-30 [1] CRAN (R 4.4.0)
+# MASS             7.3-61     2024-06-13 [1] CRAN (R 4.4.1)
+# Matrix           1.7-0      2024-04-26 [1] CRAN (R 4.4.1)
+# MatrixModels     0.5-3      2023-11-06 [1] CRAN (R 4.4.0)
+# matrixStats      1.3.0      2024-04-11 [1] CRAN (R 4.4.0)
+# maxLik           1.5-2.1    2024-03-24 [1] CRAN (R 4.4.0)
+# mcmc             0.9-8      2023-11-16 [1] CRAN (R 4.4.0)
+# MCMCpack         1.7-0      2024-01-18 [1] CRAN (R 4.4.0)
+# mded           * 0.1-2      2015-04-27 [1] CRAN (R 4.4.0)
+# miscTools        0.6-28     2023-05-03 [1] CRAN (R 4.4.0)
+# mnormt           2.1.1      2022-09-26 [1] CRAN (R 4.4.0)
+# munsell          0.5.1      2024-04-01 [1] CRAN (R 4.4.0)
+# mvtnorm          1.2-5      2024-05-21 [1] CRAN (R 4.4.0)
+# numDeriv         2016.8-1.1 2019-06-06 [1] CRAN (R 4.4.0)
+# pillar           1.9.0      2023-03-22 [1] CRAN (R 4.4.0)
+# pkgconfig        2.0.3      2019-09-22 [1] CRAN (R 4.4.0)
+# plyr             1.8.9      2023-10-02 [1] CRAN (R 4.4.0)
+# purrr          * 1.0.2      2023-08-10 [1] CRAN (R 4.4.0)
+# quantreg         5.98       2024-05-26 [1] CRAN (R 4.4.0)
+# R6               2.5.1      2021-08-19 [1] CRAN (R 4.4.0)
+# randtoolbox      2.0.4      2023-01-28 [1] CRAN (R 4.4.0)
+# RColorBrewer   * 1.1-3      2022-04-03 [1] CRAN (R 4.4.0)
+# Rcpp             1.0.12     2024-01-09 [1] CRAN (R 4.4.0)
+# readr          * 2.1.5      2024-01-10 [1] CRAN (R 4.4.0)
+# reshape2       * 1.4.4      2020-04-09 [1] CRAN (R 4.4.0)
+# rlang            1.1.4      2024-06-04 [1] CRAN (R 4.4.0)
+# rngWELL          0.10-9     2023-01-16 [1] CRAN (R 4.4.0)
+# rprojroot        2.0.4      2023-11-05 [1] CRAN (R 4.4.0)
+# RSGHB            1.2.2      2019-07-03 [1] CRAN (R 4.4.0)
+# Rsolnp           1.16       2015-12-28 [1] CRAN (R 4.4.0)
+# rstudioapi       0.16.0     2024-03-24 [1] CRAN (R 4.4.0)
+# sandwich         3.1-0      2023-12-11 [1] CRAN (R 4.4.0)
+# scales           1.3.0      2023-11-28 [1] CRAN (R 4.4.0)
+# sessioninfo    * 1.2.2      2021-12-06 [1] CRAN (R 4.4.2)
+# SparseM          1.83       2024-05-30 [1] CRAN (R 4.4.0)
+# stringi          1.8.4      2024-05-06 [1] CRAN (R 4.4.0)
+# stringr        * 1.5.1      2023-11-14 [1] CRAN (R 4.4.0)
+# survival         3.7-0      2024-06-05 [1] CRAN (R 4.4.1)
+# tibble         * 3.2.1      2023-03-20 [1] CRAN (R 4.4.0)
+# tidyr          * 1.3.1      2024-01-24 [1] CRAN (R 4.4.0)
+# tidyselect       1.2.1      2024-03-11 [1] CRAN (R 4.4.0)
+# tidyverse      * 2.0.0      2023-02-22 [1] CRAN (R 4.4.0)
+# timechange       0.3.0      2024-01-18 [1] CRAN (R 4.4.0)
+# truncnorm        1.0-9      2023-03-20 [1] CRAN (R 4.4.0)
+# tzdb             0.4.0      2023-05-12 [1] CRAN (R 4.4.0)
+# utf8             1.2.4      2023-10-22 [1] CRAN (R 4.4.0)
+# vctrs            0.6.5      2023-12-01 [1] CRAN (R 4.4.0)
+# WeightIt       * 1.1.0      2024-05-04 [1] CRAN (R 4.4.0)
+# withr            3.0.0      2024-01-16 [1] CRAN (R 4.4.0)
+# zoo              1.8-12     2023-04-13 [1] CRAN (R 4.4.0)
+#
+# [1] C:/Users/earpkin/AppData/Local/Programs/R/R-4.4.1/library
+
+# ******************************************************************
+# Setup Environment: ####
+# ******************************************************************
+
+
+library(dplyr)
+library(magrittr)
+library(reshape2)
+library(mded)
+library(here)
+library(data.table)
+library(tidyverse)
+library(WeightIt)
+library(cobalt)
+library(tidyr)
+library(apollo)
+library(ggridges)
+library(ggplot2)
+library(distributional)
+library(ggdist)
+library(gridExtra)
+library(RColorBrewer)
+
+
+# ******************************************************************
+#### Section 1: Create EB table ####
+## NOTE, this script does NOT setup data from scratch
+# ******************************************************************
+
+
+# Table 2:Differences in mean pre and post balancing across relevant variables for respondents reporting confidence in experts of level 1 /
+#   5 on the Likert scale. Remaining differences statistically very small
+## TODO: Fix row order to match paper
+here("OtherScripts/EntropyBalancing", "01_Experts_Table2_PrePostBalancing.R") %>% source()
+
+
+
+# ******************************************************************
+#### Section 2: Hypothesis 1 ####
+# ******************************************************************
+
+here("OtherScripts/Figures", "02_Experts_Figure1_SQChoices.R") %>% source()
+here("OtherScripts/Tables", "03_Experts_Table3_SQChoices_Chisq.R") %>% source()
+
+
+## This needs fixing
+here("OtherScripts/Tables", "XX_Experts_InText_ChoiceFrequency.R") %>% source()
+
+
+# ******************************************************************
+#### Section 3: Hypothesis 2 ####
+# ******************************************************************
+
+
+here("OtherScripts/Figures", "04_Experts_Figure2_ConfidenceCertainty.R") %>% source()
+here("OtherScripts/Tables", "05_Experts_Table4_ConfidenceCertainty_Chisq.R") %>% source()
+
+
+# ******************************************************************
+#### Section 4: Hypothesis 3 ####
+# ******************************************************************
+
+
+here("CEModelScripts", "06_Experts_Unweighted_MXL_V2") %>% source()
+here("CEModelScripts", "07_Experts_Weighted_MXL_V2.R") %>% source()
+
+here("OtherScripts/Figures", "08_Experts_Figure3_WTP.R") %>% source()
+
+here("OtherScripts/Tables", "09_Experts_Table5_ModelOutputs.R") %>% source()
+
+## Check version names here
+here("OtherScripts/Tables", "10_Experts_Table6_MWTestByConfidence.R") %>% source()
+
+
+# ******************************************************************
+#### Section 5: Appendix B Figures ####
+# ******************************************************************
+
+## TODO change these to B
+here("OtherScripts/Figures", "11_Experts_FigureB1_ExpertsHistogram.R") %>% source()
+
+## Figure B2
+here("OtherScripts/Figures", "12_Experts_FigureB2_ChoicesConfidence_Facet.R") %>% source()
+
+## Figure B3
+here("OtherScripts/Figures", "13_Experts_FigureB3_RisksByExperts.R") %>% source()
+
+
+# ******************************************************************
+#### Section 6: Appendix B Tables ####
+# ******************************************************************
+
+## Table B1
+here("OtherScripts/EntropyBalancing", "14_Experts_TableB1_EBBalancing.R") %>% source()
+
+## Table B2
+here("OtherScripts/Tables", "15_Experts_TableB2_SampleTest.R") %>% source()
+
+## Table B3
+here("OtherScripts/Tables", "16_Experts_TableB3_FullModelOutputs.R") %>% source()
+
+## Table B4
+here("OtherScripts/Tables", "17_Experts_TableB4_WTPTable.R") %>% source()
+
+## Table B5
+here("OtherScripts/Tables", "18_Experts_TableB5_FrequencyOfChoices.R") %>% source()
+
+
+
+# END OF SCRIPT **********************************************
